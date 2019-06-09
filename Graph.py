615519177949254fb53372fd17f47ed9cae25e38
Centurion256 <Vkuchynskyy@gmail.com>
2019-06-10 02:07:03 +0300

    Added support for colours.

--- Graph.py
+++ Graph.py
@@ -1,13 +1,14 @@
 class ColouredGraph(object):
 
-    def __init__(self, matrix):
+    def __init__(self, matrix, colours):
 
         self._adjacency_list = self.matrix_to_dict(matrix)
+        self.colours = colours
 
     @staticmethod
     def matrix_to_dict(matrix):
 
-        return {col: {row for row in range(len(matrix[col])) if matrix[col][row] != 0} for col in range(len(matrix))}
+        return {col: {'adjacent':{row for row in range(len(matrix[col])) if matrix[col][row] != 0}, 'colour': None} for col in range(len(matrix))}
 
     def __str__(self):
 
@@ -16,5 +17,5 @@ class ColouredGraph(object):
 if __name__ == "__main__":
     
     matrix = [[1,1,0],[0,0,1],[1,0,0]]
-    graph = ColouredGraph(matrix)
+    graph = ColouredGraph(matrix, {"R", "G", "B", "Y"})
     print(graph)
