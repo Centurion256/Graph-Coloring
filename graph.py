caf5fb479c187eb5ea79ee4efc9940c7707ebe40
ddatsko <ddatsko010101@gmail.com>
2019-06-11 22:54:11 +0300

    added some documentation

--- graph.py
+++ graph.py
@@ -3,26 +3,30 @@ from igraph import Graph, plot
 
 
 class ColouredGraph:
-    colors_list = ["#FC4040", "#40FC40", "#4040FC", "#FCFC40", "#FC40FC", "#40FCFC", "#B57955"]
-    """ 
+    """
     A data structure for representing coloured graphs.
     """
+    colors_list = ["#FC4040", "#40FC40", "#4040FC", "#FCFC40", "#FC40FC", "#40FCFC", "#B57955"]
 
-    def __init__(self, matrix, colours):
-        self._adjacency_list = self.matrix_to_dict(matrix)
-        self.colours = ColouredGraph.colors_list[:colours]
+    def __init__(self, matrix, colours_number):
+        """
+        :param matrix: adjacency matrix representing a graph
+        :param colours_number: nu,ber of colours to colour graph into
+        """
+        self._adjacency_list = self.matrix_to_list(matrix)
+        self.colours = ColouredGraph.colors_list[:colours_number]
 
     @staticmethod
-    def matrix_to_dict(matrix):
+    def matrix_to_list(matrix):
         """
-        Converts an adjancency matrix to an adjacency list(dict).
+        Converts an adjancency matrix to an adjacency list.
 
         :param matrix: An adjacency matrix.
         :returns: A dictionary object that represents an adjacency list.
         :rtype: dict
         """
-        return {col: {'adjacent': {row for row in range(len(matrix[col])) if matrix[col][row] != 0}, 'colour': None} for
-                col in range(len(matrix))}
+        return [{'adjacent': {row for row in range(len(matrix[col])) if matrix[col][row] != 0}, 'colour': None} for
+                col in range(len(matrix))]
 
     def __str__(self):
         return "\n".join(f"{x}: {self._adjacency_list[x]}" for x in self._adjacency_list)
@@ -43,22 +47,33 @@ class ColouredGraph:
         """
         if color not in self.colours:
             raise KeyError(f"Colour \'{color}\' is not available for this graph.")
+
+        # if any adkacent vertex already has the same colour
         if any(self._adjacency_list[adjacent]['colour'] == color for adjacent in
                self._adjacency_list[node]['adjacent']):
             raise ValueError(
                 "Either one of the adjacent nodes has the same color or the node itself is already colored.")
+
+        # color the vertex
         self._adjacency_list[node]['colour'] = color
         return True
 
     def __color_helper(self, vertex=0):
         if vertex == len(self._adjacency_list):
+            # if all the vertices are coloured already
             return True
 
+        # iterate over all the possible colours
         for color in self.colours:
             try:
+                # try to colour in given color
                 self.colour(vertex, color)
+
+                # recurse to the next vertex
                 if self.__color_helper(vertex + 1):
                     return True
+
+                # clear the colour while going back to the previous vertex
                 self._adjacency_list[vertex]['colour'] = None
             except ValueError:
                 continue
@@ -66,11 +81,15 @@ class ColouredGraph:
 
     def color_graph(self):
         """ 
-        Colours graph with colours privided.
+        Colours graph with colours pri]ovided.
         """
+        # create new graph object not to mekt the method destructive
         graph = copy.deepcopy(self)
+
+        # color the graph
         status = graph.__color_helper()
         if status is True:
+            # replace all the Nones to colors names in the initial graph
             self._adjacency_list = graph._adjacency_list
         return status
 
